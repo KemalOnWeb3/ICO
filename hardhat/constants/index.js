54ccb901192370d5aee40e79a8be7389ee065df0
Kemal Cleva <68566006+KemalOnWeb3@users.noreply.github.com>
2022-05-24 20:41:46 +0200

    Initial commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x0353eF3ADc48559d8dDb782599bb614B93E24f56";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
